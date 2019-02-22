41eb1a0e74e1e912282c2e4d215a07b699ac20e2
丁丁 <dingding@bxb.tw>
2019-02-22 10:23:36 +0800

    add english version

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -12,7 +12,7 @@
       </FlexboxLayout>
       <Label :text="display_num" class="display"></Label>
       <Label :text="'play:'  + audioPath"></Label>
-      <Button v-if="showStopbtn" @tap="clickStop" text="暫停"></Button>
+      <Button v-if="showStopbtn" @tap="clickStop" text="Stop"></Button>
     </StackLayout>
   </Page>
 </template>
@@ -34,14 +34,15 @@ export default {
     pickFile() {
       let options = {
         android: {
-          maxNumberFiles: 1, // 只接受挑選一個檔案
-          extensions: ["mp3"] //只接受 *.mp3
+          maxNumberFiles: 1, // Only pick 1 file a time. 只接受挑選一個檔案 
+          extensions: ["mp3"] // // Only pick 1 file a time. 只接受 *.mp3
         }
       };
       let mpicker = new Mediafilepicker();
       mpicker.openFilePicker(options);
       mpicker.on("getFiles", res => {
         let results = res.object.get("results");
+        // the file path is results[0].file, set it to `this.audioPath`
         // 選到的檔案路徑為 results[0].file，設定到 `this.audioPath` 裡
         this.audioPath = results[0].file;
         console.log("Set path: ", this.audioPath);
@@ -58,13 +59,13 @@ export default {
     count_down(num) {
       if (this.c_id) clearInterval(this.c_id);
       this.display_num = num;
-      this.showStopbtn = true //顯示「Stop」按鈕
+      this.showStopbtn = true //When start counting, shows Stop button 顯示「Stop」按鈕
       this.c_id = setInterval(() => {
         this.display_num = this.display_num - 1;
         if (this.display_num === 0) {
           this.display_num = `Time's up`;
-          this.play(); //播放音樂
-          clearInterval(this.c_id); //把計時器清空
+          this.play(); //play music  播放音樂
+          clearInterval(this.c_id); //Count down finished, stop counting. 把計時器清空
         }
       }, 1000);
     },
@@ -72,6 +73,7 @@ export default {
       if (!this.audioPath) return;
       this.player = new TNSPlayer();
       const playerOptions = {
+         // Set the music file path (pickupFile from Step 4.1) to `audioFile`
         // 把 4.1 選到的 audioPath 指定給 audioFile 參數
         audioFile: this.audioPath,
         loop: false,
